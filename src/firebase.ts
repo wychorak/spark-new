010bc871747e68ef5feae60dc05a3c5ac04bb52d
wychorak <wychor234@gmail.com>
2026-06-30 16:21:37 +0200

    Add Firebase config fallbacks for release builds

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,15 +2,26 @@ import { getApp, getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+const firebaseDefaults = {
+  apiKey: "AIzaSyDs-x9Fe_m11y3uYKEKCSc0t2VCnTtBDbU",
+  authDomain: "spark-70b03.firebaseapp.com",
+  projectId: "spark-70b03",
+  storageBucket: "spark-70b03.firebasestorage.app",
+  messagingSenderId: "271339297035",
+  appId: "1:271339297035:web:cdb95d58b3e84a44272a0e",
+  iosBundleId: "com.sparknew.connect",
+  androidPackageName: "com.sparknew.connect"
+};
+
 const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  iosBundleId: process.env.EXPO_PUBLIC_FIREBASE_IOS_BUNDLE_ID,
-  androidPackageName: process.env.EXPO_PUBLIC_FIREBASE_ANDROID_PACKAGE_NAME
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY || firebaseDefaults.apiKey,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN || firebaseDefaults.authDomain,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID || firebaseDefaults.projectId,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET || firebaseDefaults.storageBucket,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || firebaseDefaults.messagingSenderId,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID || firebaseDefaults.appId,
+  iosBundleId: process.env.EXPO_PUBLIC_FIREBASE_IOS_BUNDLE_ID || firebaseDefaults.iosBundleId,
+  androidPackageName: process.env.EXPO_PUBLIC_FIREBASE_ANDROID_PACKAGE_NAME || firebaseDefaults.androidPackageName
 };
 
 const missingConfig = Object.entries(firebaseConfig)
